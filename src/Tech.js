17f5da0a990a02e1c36c7d1c98cd32f798d81c43
Rohit Yadav <developer.royad@gmail.com>
2023-09-22 22:19:40 +0530

    tech_stack

--- src/Tech.js
+++ src/Tech.js
@@ -4,7 +4,7 @@ const Tech = () => {
     return (
         <>
             <div className='h-auto mx-2 p-2'>
-                <div className="text-2xl font-[700]">Tech Stuff</div>
+                <div className="text-2xl font-[700]">Tech Stack</div>
                 <div className='flex flex-wrap justify-between items-center py-6 gap-3'>
                     <p className='border border-[#2b3031] text-[#adb0b1] text-sm p-2 rounded'>HTML, CSS, JavaScript, Node.js</p>
                     <p className='border border-[#2b3031] text-[#adb0b1] text-sm p-2 rounded'>React.js, Bootstrap, Tailwind CSS</p>
